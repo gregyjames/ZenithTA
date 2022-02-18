c4bda42078112e9f5041c1eb630881cec888d370
Greg <gregyjames@users.noreply.github.com>
2022-02-18 02:45:18 -0600

    Wrapped the ema,sma,rsi, and macd with PyO3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Rust](https://github.com/gregyjames/Panther/actions/workflows/rust.yml/badge.svg?branch=main&event=push)](https://github.com/gregyjames/Panther/actions/workflows/rust.yml)
 
 # Panther
-A high performance python technical analysis library written in Rust and rust-numpy. Plans to wrap and turn into a python library in the future with PyO3.
+A efficient, high-performance python technical analysis library written in Rust using PyO3 and rust-numpy. 
 
 ## Indicators
 - SMA
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,56 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
 use numpy::ndarray::prelude::*;
 
-fn sma(prices: &Array1<f32>, period: usize) -> Array1<f32> {
-    let length = prices.len() - period +1;
+#[pyfunction]
+fn sma(price: Vec<f32>, period: usize) -> PyResult<Vec<f32>> {
+    let price_ndarray = Array::from_vec(price);
+    let length = price_ndarray.len() - period +1;
     let mut result = Array1::<f32>::zeros(length);
 
     for i in 0..length {
-        let slice = prices.slice(s![i..i+period]);
+        let slice = price_ndarray.slice(s![i..i+period]);
         result[i] = slice.sum()/(period as f32);
     }
 
-    result
+    Ok(Array::to_vec(&result))
 }
 
-fn ema(prices: &Array1<f32>, period: usize) -> Array1<f32>{
-    let length = prices.len() - period +1;
+fn ema_helper(price_ndarray: &Array1<f32>, period: usize) -> Array1<f32>{
+    let length = price_ndarray.len() - period +1;
     let mut result = Array1::<f32>::zeros(length);
-    result[0] = prices.slice(s![0..period]).sum();
+    result[0] = price_ndarray.slice(s![0..period]).sum();
     for i in 1..result.len(){
-        result[i] = result[i-1]+(prices[i+period-1]-result[i-1])*(2.0/((period as f32)+1.0));
+        result[i] = result[i-1]+(price_ndarray[i+period-1]-result[i-1])*(2.0/((period as f32)+1.0));
     }
+    
     result
 }
 
-fn rsi(prices: &Array1<f32>, period: usize) -> Array1<f32>{
-    let mut change = Array1::<f32>::zeros(prices.len());
-    let mut gain = Array1::<f32>::zeros(prices.len());
-    let mut loss = Array1::<f32>::zeros(prices.len());
-    let mut ag = Array1::<f32>::zeros(prices.len());
-    let mut al = Array1::<f32>::zeros(prices.len());
-    let mut result = Array1::<f32>::zeros(prices.len());
-    for i in 1..prices.len(){
-        change[i] = prices[i] - prices[i-1];
+#[pyfunction]
+fn ema(price: Vec<f32>, period: usize) -> PyResult<Vec<f32>>{
+    let price_ndarray = Array::from_vec(price);
+    let length = price_ndarray.len() - period +1;
+    let mut result = Array1::<f32>::zeros(length);
+    result[0] = price_ndarray.slice(s![0..period]).sum();
+    for i in 1..result.len(){
+        result[i] = result[i-1]+(price_ndarray[i+period-1]-result[i-1])*(2.0/((period as f32)+1.0));
+    }
+    
+    Ok(Array::to_vec(&result))
+}
+
+#[pyfunction]
+fn rsi(price: Vec<f32>, period: usize) -> PyResult<Vec<f32>>{
+    let price_ndarray = Array::from_vec(price);
+    let mut change = Array1::<f32>::zeros(price_ndarray.len());
+    let mut gain = Array1::<f32>::zeros(price_ndarray.len());
+    let mut loss = Array1::<f32>::zeros(price_ndarray.len());
+    let mut ag = Array1::<f32>::zeros(price_ndarray.len());
+    let mut al = Array1::<f32>::zeros(price_ndarray.len());
+    let mut result = Array1::<f32>::zeros(price_ndarray.len());
+    for i in 1..price_ndarray.len(){
+        change[i] = price_ndarray[i] - price_ndarray[i-1];
         if change[i] == 0.0{
             gain[i] = 0.0;
             loss[i] = 0.0;
@@ -45,19 +65,33 @@ fn rsi(prices: &Array1<f32>, period: usize) -> Array1<f32>{
     ag[period] = gain.slice(s![1..period+1]).sum()/(period as f32);
     al[period] = loss.slice(s![1..period+1]).sum()/(period as f32);
     
-    for i in period+1..prices.len(){
+    for i in period+1..price_ndarray.len(){
         ag[i] = (ag[i-1]*(period as f32-1.0)+gain[i])/period as f32;
         al[i] = (al[i-1]*(period as f32-1.0)+loss[i])/period as f32;
     }
-    for i in period+1..prices.len(){
+    for i in period+1..price_ndarray.len(){
         result[i] = 100.0-(100.0/(1.0+ag[i]/al[i]))
     }
-    result.slice(s![period..]).to_owned()
+
+    //result.slice(s![period..]).to_owned()
+    Ok(Array::to_vec(&(result.slice(s![period..]).to_owned())))
 }
 
-fn macd(prices: &Array1<f32>, period_fast: usize, period_slow: usize, period_signal: usize) -> (Array1<f32>,Array1<f32>){
-    let line = ema(prices, period_fast).slice(s![period_slow-period_fast..]).to_owned() - ema(prices,period_slow);
-    let signal = ema(&line, period_signal);
+#[pyfunction]
+fn macd(price: Vec<f32>, period_fast: usize, period_slow: usize, period_signal: usize) -> PyResult<(Vec<f32>,Vec<f32>)>{
+    let price_ndarray = Array::from_vec(price);
+    let line = ema_helper(&price_ndarray, period_fast).slice(s![period_slow-period_fast..]).to_owned() - ema_helper(&price_ndarray,period_slow);
+    let signal = ema_helper(&price_ndarray, period_signal);
 
-    return (line, signal)
+    Ok((Array::to_vec(&line), Array::to_vec(&signal)))
 }
+
+#[pymodule]
+fn panther(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sma, m)?)?;
+    m.add_function(wrap_pyfunction!(ema, m)?)?;
+    m.add_function(wrap_pyfunction!(rsi, m)?)?;
+    m.add_function(wrap_pyfunction!(macd, m)?)?;
+    Ok(())
+}
+
