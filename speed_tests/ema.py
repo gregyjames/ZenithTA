366c993b81dd3a9b8217f55a24a2fe082d633208
Greg <gregyjames@users.noreply.github.com>
2022-02-18 03:20:06 -0600

    Added EMA and SMA speed tests!

---
+++ speed_tests/ema.py
@@ -0,0 +1,21 @@
+import pandas_datareader as pdr
+import pandas as pd
+from panther import *
+from timeit import default_timer as timer
+from datetime import timedelta
+
+data = pdr.get_data_yahoo('NVDA')
+
+print("Timing Panther:")
+start = timer()
+a = data['Close'].tolist()
+j = ema(a,4)
+end = timer()
+print(timedelta(seconds=end-start))
+
+print("Timing Pandas:")
+start = timer()
+data['4dayEWM'] = data['Close'].ewm(span=4, adjust=False).mean()
+b = data['4dayEWM'].tolist()
+end = timer()
+print(timedelta(seconds=end-start))
---
+++ speed_tests/sma.py
@@ -0,0 +1,21 @@
+import pandas_datareader as pdr
+import pandas as pd
+from panther import *
+from timeit import default_timer as timer
+from datetime import timedelta
+
+data = pdr.get_data_yahoo('NVDA')
+
+print("Timing Panther:")
+a = data['Close'].tolist()
+start = timer()
+j = sma(a,5)
+end = timer()
+print(timedelta(seconds=end-start))
+
+print("Timing Pandas:")
+start = timer()
+data['SMA(5)'] = data.Close.rolling(5).mean()
+end = timer()
+b = data['SMA(5)'].tolist()
+print(timedelta(seconds=end-start))
